d459c8f54fef94a950c5f036f9b2ecada91fc73f
Allan Souza <allan.souza@blueshift.com.br>
2021-05-08 07:17:45 -0300

    add validation in exercise 04_mix_up

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -15,7 +15,8 @@ Assuma que a e b tem tamanho 2 ou maior.
 
 def mix_up(a, b):
     # +++ SUA SOLUÇÃO +++
-    return ''.join((b[:2], a[2:], ' ', a[:2], b[2:]))
+    return ''.join((b[:2], a[2:], ' ', a[:2], b[2:])) \
+        if len(a) > 1 < len(b) else 'parameters must be contain more than 1 caracteres'
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
@@ -43,3 +44,4 @@ if __name__ == '__main__':
     test(mix_up, ('dog', 'dinner'), 'dig donner')
     test(mix_up, ('gnash', 'sport'), 'spash gnort')
     test(mix_up, ('pezzy', 'firm'), 'fizzy perm')
+    test(mix_up, ('p', 'f'), 'parameters must be contain more than 1 caracteres')
